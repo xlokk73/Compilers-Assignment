6f07c3aa4f30f42f41452222dfb1ad888d5340fa
Manwel Bugeja <manwelbugeja@Manwels-MBP.lan>
2020-09-17 21:17:55 +0200

    finished

--- src/visitor/semantic_analyser.cpp
+++ src/visitor/semantic_analyser.cpp
@@ -1,1 +1,15 @@
+#include "semantic_analysis.h"
 
+void Scope::already_declared(std::string identifier) {
+    return variable_symbol_table.find(identifier) != variable_symbol_table.end();
+}
+
+void Scope::declare(std::string identifier, TYPE type) {
+    variable_symbol_table[identifier] = type;
+
+TYPE Scope::type(std::string identifier) {
+    if (already_declared(identifier) 
+        return variable_symbol_table[identifier];
+    
+    std::cout << "Error during semantic analysis" << std::endl;
+}
--- src/visitor/semantic_analyser.h
+++ src/visitor/semantic_analyser.h
@@ -1,14 +1,19 @@
 #include <map>
 #include <vecotr>
 #include <stack>
+#include <utility>
+#include <iostream>
 #include "../parser/ast.h"
 #include "../parser/parser.h"
 
 class Scope {
 public:
-    bool declare(std::string, TYPE);
-    void declare(std::string, TYPE, std::vector<TYPE>);
+    bool already_declared(std::string);
+    void declare(std::string, TYPE);
+    TYPE type(std::string);
 
 private:
     std::map<std::string, std::pair<parser::TYPE, unsigned int>> variable_symbol_table;
     
+}
+       
